5ae2140da6f79b061ec221e0c8738b9e7e08329f
pooja <pooja@gamil.com>
2024-08-28 08:57:52 +0530

    initioal setup

---
+++ controllers/todo.controller.js
@@ -0,0 +1,24 @@
+const asyncHandler = require("express-async-handler")
+const Todo = require("../modal/Todo")
+const { io } = require("../socket/socket")
+
+exports.createTodo = asyncHandler(async (req, res) => {
+    await Todo.create(req.body)
+    const result = await Todo.find()
+    io.emit("todo-create-responce", result)
+    res.json({ message: "createTodo success" })
+})
+exports.readTodo = asyncHandler(async (req, res) => {
+    const result = await Todo.find()
+    res.json({ message: "readTodo success", result })
+})
+exports.updateTodo = asyncHandler(async (req, res) => {
+    const result = await Todo.findByIdAndUpdate(req.params._id, req.body)
+    res.json({ message: "updateTodo success" })
+})
+exports.deleteTodo = asyncHandler(async (req, res) => {
+    await Todo.findByIdAndDelete(req.params.id)
+    const result = await Todo.find()
+    io.emit("todo-create-responce", result)
+    res.json({ message: "deleteTodo success" })
+})
